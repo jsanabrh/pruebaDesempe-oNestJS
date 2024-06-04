84ad37c499da4783917a94cfc02b901d1bd2ce9c
jsanabrh <juanhoyos0422@gmail.com>
2024-06-04 10:58:43 -0500

    The API responses are declared for the statuses of the request.

--- src/results/controller/result.controller.ts
+++ src/results/controller/result.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ResultService } from '../service/result.service';
 import { CreateResultDto } from '../dto/result.dto';
 import { ResultEntity } from '../entities/result.entity';
@@ -9,6 +9,15 @@ import { ResultEntity } from '../entities/result.entity';
 export class ResultController {
   constructor(private readonly resultService: ResultService) {}
 
+  @ApiResponse({ status: 201, description: 'Result created successfully.' })
+  @ApiResponse({
+    status: 400,
+    description: 'The data entered to create the result is invalid.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An internal server error occurred while creating the result.',
+  })
   @Post('/createResult')
   async createResult(
     @Body() createResult: CreateResultDto,
--- src/tournaments/controller/tournament.controller.ts
+++ src/tournaments/controller/tournament.controller.ts
@@ -7,7 +7,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TournamentService } from '../service/tournament.service';
 import { CreateTournamentDto } from '../dto/createTournament.dto';
 import { TournamentEntity } from '../entities/tournament.entity';
@@ -17,6 +17,16 @@ import { TournamentEntity } from '../entities/tournament.entity';
 export class TournamentController {
   constructor(private readonly tournamentService: TournamentService) {}
 
+  @ApiResponse({ status: 201, description: 'Tournament created successfully.' })
+  @ApiResponse({
+    status: 400,
+    description: 'The data entered to create the tournament is invalid.',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'An internal server error occurred while creating the tournament.',
+  })
   @Post('/createTournament')
   async createTournament(
     @Body() createTournament: CreateTournamentDto,
@@ -24,11 +34,34 @@ export class TournamentController {
     return this.tournamentService.createTournament(createTournament);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'All tournaments were found successfully.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No tournaments were found in the system.',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'An internal server error occurred while searching for the tournaments.',
+  })
   @Get('findAllTournament')
   async findAllTournaments() {
     return this.tournamentService.findAllTournament();
   }
 
+  @ApiResponse({ status: 200, description: 'Tournament updated successfully.' })
+  @ApiResponse({
+    status: 404,
+    description: 'Tournament with the entered ID not found.',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'An internal server error occurred while updating the tournament.',
+  })
   @Put('/updateTournament:idTournament')
   async updateUser(
     @Param('idTournament') idTournament: number,
@@ -40,6 +73,16 @@ export class TournamentController {
     );
   }
 
+  @ApiResponse({ status: 200, description: 'Tournament deleted successfully.' })
+  @ApiResponse({
+    status: 404,
+    description: 'Tournament with the entered ID not found.',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'An internal server error occurred while deleting the tournament.',
+  })
   @Delete('/deleteTournament/:idTournament')
   remove(@Param('idTournament') idTournament: number) {
     return this.tournamentService.removeTournament(+idTournament);
